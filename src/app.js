4498491cc1df8fc6432a660786c35d250e91f1af
sxcw <yuan329419@gmail.com>
2017-08-01 22:21:06 -0500

    Auth - Include Firebase to app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
+import firebase from 'firebase';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: '',
+      authDomain: 'auth-dbf1b.firebaseapp.com',
+      databaseURL: 'https://auth-dbf1b.firebaseio.com',
+      projectId: 'auth-dbf1b',
+      storageBucket: 'auth-dbf1b.appspot.com',
+      messagingSenderId: ''
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>App!</Text>
+        <Header headerText='authentication' />
+        <LoginForm />
       </View>
-    )
+    );
   }
 }
 
