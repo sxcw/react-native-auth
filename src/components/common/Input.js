46f8d5639dd1fe825047b68b9b643ad4e274a7a2
sxcw <yuan329419@gmail.com>
2017-08-06 22:49:11 -0500

    Auth - Add password field with secureTextEntry

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput } from 'react-native';
 
-const Input = ({ label, value, onChangeText, placeholder }) => {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
   const { inputStyle, labelStyle, containerStyle } = styles;
   return (
     <View style={containerStyle}>
       <Text style={labelStyle}>{label}</Text>
       <TextInput
+        secureTextEntry={secureTextEntry}
         autoCorrect={false}
         placeholder={placeholder}
         style={inputStyle}
